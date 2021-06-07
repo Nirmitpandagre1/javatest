2c740e85d617244a5339e7a726bd9fe52a26da63
Nirmitpandagre1 <47783800+Nirmitpandagre1@users.noreply.github.com>
2021-06-07 15:45:32 +0530

    Add files via upload

---
+++ Exercise2.java
@@ -0,0 +1,8 @@
+public class Ex1 {
+public static void main(String[] args) {     String str=
+int sum = 0;
+for (int i : my_array)
+    sum += i;
+System.out.println("The sum is " + sum);
+}
+}
---
+++ Exercise4.java
@@ -0,0 +1,12 @@
+public class Exercise4 {
+public static void main(String[] args) {    
+       int[] numbers = new int[]{3,4,5,7,9};
+       //calculate sum of all array elements
+       int sum = 0;
+       for(int i=0; i < numbers.length ; i++)
+        sum = sum + numbers[i];
+       //calculate average value
+        double average = sum / numbers.length;
+        System.out.println("Average value s : " + average); 
+   }
+}
